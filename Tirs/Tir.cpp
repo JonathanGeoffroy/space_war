ce3c85f060dd29c2fbbdf39c02bdf80e7aa976eb
Jonathan <geoffroy.jonathan@gmail.com>
2012-09-04 11:14:26 +0200

    change initial position when an ennemy shoot

--- Tirs/Tir.cpp
+++ Tirs/Tir.cpp
@@ -12,7 +12,10 @@ Tir::Tir(int dir, int rotat, int x, int y) : Affichable(dir, rotat) {
 }
 Tir::Tir(Vaisseau* v) : Affichable(v->getDirection(), v->GetRotation()) {
 	SetImage(img);
-	SetPosition(v->GetPosition().x, v->GetPosition().y - (v->GetSize().y / 2) - GetSize().y);
+	if(v->getDirection() == HAUT)
+		SetPosition(v->GetPosition().x, v->GetPosition().y - (v->GetSize().y / 2) - (GetSize().y / 2));
+	else
+		SetPosition(v->GetPosition().x, v->GetPosition().y + (v->GetSize().y / 2) + (GetSize().y / 2));
 	SetCenter(GetSize().x / 2, GetSize().y / 2);
 }
 Tir::~Tir() {}
